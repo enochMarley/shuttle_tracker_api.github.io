aad1b0d9378cc47a4a7abb3dc35eaf6b744a62c3
Enoch Sowah <enoch.sowah16@gmail.com>
2018-05-29 10:03:18 +0000

    another commit

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ var busDetails = mongoose.model("coordinates", busCoordsSchema);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
+
 
 app.get('/', (req,res) => {
 	res.send({'success': 'true'})
